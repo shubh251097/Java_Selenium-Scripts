b422ca30f3bb8464566c4118ae82b425bcddf1cf
Shubham Sultane <sultaneshubham1996@gmail.com>
2025-08-22 14:30:44 +0530

    First Commit

--- src/arrays/array1.java
+++ src/arrays/array1.java
@@ -5,35 +5,34 @@ import java.util.Arrays;
 public class array1 {
 
 	public static void main(String[] args) {
-		int a[]= {5,10,15,20,25};
-		System.out.println(a.length);
-		System.out.println(Arrays.toString(a));
-		//System.out.println(a[7]);
+		// int arr[] = new int[5];
+		int arr[] = { 2, 5, 7, 9, 15 };
+
+		// int arr[]= {5,10,15,20,25};
+		System.out.println(arr.length);
+		System.out.println(Arrays.toString(arr));
+		// System.out.println(a[7]);
 		try {
-		System.out.println(a[7]);
-		}
-		catch(Exception e) {
+			System.out.println(arr[3]);
+		} catch (Exception e) {
 			System.out.println("ArrayIndexOutofBoundException");
 		}
-		
-		
-		
-System.out.println(a[2]);
-for(int k:a) {
-	System.out.print(k+ " ");
-}
-System.out.println();
 
-for (int i=0;i<a.length;i++)
-{
-	System.out.println(a[i]);
-}
-a[2]=6;
-a[4]=28;
-System.out.println(a[4]);
-for(int k:a) {
-	System.out.print(k+ " ");
-}
+		System.out.println(arr[2]);
+		for (int k : arr) {
+			System.out.print(k + " ");
+		}
+		System.out.println();
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		arr[2] = 6;
+		arr[4] = 28;
+		System.out.println(arr[4]);
+		for (int k : arr) {
+			System.out.print(k + " ");
+		}
 
 	}
 
--- src/selenium_scripts/Basic.java
+++ src/selenium_scripts/Basic.java
@@ -5,48 +5,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Basic{
+public class Basic {
 
 	public static void main(String[] args) throws InterruptedException {
-	
 
+		WebDriver driver = new ChromeDriver();
 
-        WebDriver driver = new ChromeDriver();
-        // to launch the browser
-        
-        driver.get("https://www.google.co.in/");
-        // to launch the url/test page
-        
-        driver.manage().window().maximize();
-        // maximize the browser window
-        
-        Thread.sleep(2000);
-        
-        //WebElement searchBar = driver.findElement(By.id("APjFqb"));
-        WebElement searchBar = driver.findElement(By.xpath("//textarea[@name='q']"));
-        // identify or locate the web element
-        
-        System.out.println(searchBar.isDisplayed());
-        
-        System.out.println(searchBar.isEnabled());
-        
-        searchBar.click();
-        // perform click operation....
-        
-        Thread.sleep(2000);
-        
-        searchBar.sendKeys("squareit");
-        
-        Thread.sleep(2000);
-        
-        searchBar.clear();
-        
-        Thread.sleep(2000);
-        
-        driver.close();
-        
+		driver.get("https://www.google.com/");
+
+		driver.manage().window().maximize();
+
+		Thread.sleep(2000);
+
+		// WebElement searchBar = driver.findElement(By.id("APjFqb"));
+		// WebElement searchBar = driver.findElement(By.xpath("//textarea[@name='q']"));
+		// driver.findElement(By.linkText("Gmail")).click();
+
+		WebElement search = driver.findElement(By.name("q"));
+
+		boolean b1 = search.isDisplayed();
+		boolean b2 = search.isEnabled();
+		System.out.println(b1);
+		System.out.println(b2);
+
+		Thread.sleep(2000);
+		if (search.isDisplayed() && search.isEnabled()) {
+			search.sendKeys("India");
+			search.submit();
+
+		}
+
+		// searchBar.clear();
+
+		// Thread.sleep(2000);
+
+		// driver.close();
+
 	}
 
 }
-
-
