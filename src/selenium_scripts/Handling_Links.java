916305ee4d2edf7258cb27da1860d55d556f3f3b
Shubham Sultane <sultaneshubham1996@gmail.com>
2026-02-04 23:52:59 +0530

    code updated

--- src/selenium_scripts/Handling_Links.java
+++ src/selenium_scripts/Handling_Links.java
@@ -12,49 +12,38 @@ public class Handling_Links {
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.get("https://login.salesforce.com/?locale=in");
-		
+
 		Thread.sleep(2000);
-		
-		
+
 		WebElement link1 = driver.findElement(By.linkText("Forgot Your Password?"));
-		//link1.click();
+		// link1.click();
 		System.out.println(link1.isDisplayed());
 		System.out.println(link1.isEnabled());
-		System.out.println(link1.isSelected());
-		
+		// System.out.println(link1.isSelected());
+
 		link1.click();
 		String actualURL = driver.getCurrentUrl();
 		String expectedURL = "https://login.salesforce.com/secur/forgotpassword.jsp?locale=in&lqs=locale%3Din";
-        
+
 		System.out.println(actualURL.equalsIgnoreCase(expectedURL));
-		
+
 		Thread.sleep(2000);
-		
+
 		WebElement link2 = driver.findElement(By.partialLinkText("Need Help Loggi"));
 		boolean b1 = link2.isDisplayed();
-		if(b1==true)
-		{
+		if (b1 == true) {
 			System.out.println("passed");
-		}
-		else
-		{
+		} else {
 			System.out.println("failed...!!");
 			System.out.println("element is not displayed on screen");
 		}
-		
+
 		boolean b2 = link2.isEnabled();
-		if(b2==true)
-		{
+		if (b2 == true) {
 			System.out.println("link is displayed");
-		}
-		else
-		{
+		} else {
 			System.out.println("link is not displayed!");
 		}
 	}
 
-
-
-	}
-
-
+}
